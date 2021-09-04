13b133cfc5692d66b3b70f399c717a523fe9e3cc
Dreezy305 <bankoleidris@gmail.com>
2021-09-04 21:06:11 +0000

    first commit

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // modules - Escapulated code (only share minimum)
 // every file is a module by default
+// common js, every file is module (by default)
+
+sayHi("idris");
+sayHi(secret);
+sayHi(john);
+sayHi(peter);
