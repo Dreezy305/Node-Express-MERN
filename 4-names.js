8142c36e07d5583f0ea034ecb297b1ca9f02f73e
Dreezy305 <bankoleidris@gmail.com>
2021-09-04 21:25:15 +0000

    first commit

--- 4-names.js
+++ 4-names.js
@@ -4,12 +4,3 @@
 const secret = "SUPER SECRET";
 const john = "john";
 const peter = "peter";
-
-const sayHi = (name) => {
-  console.log(`Hello there ${name}`);
-};
-
-sayHi("idris");
-sayHi(secret);
-sayHi(john);
-sayHi(peter);
