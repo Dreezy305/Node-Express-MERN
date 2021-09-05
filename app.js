7df39fce913656709fc24a122af459657d27e039
Dreezy305 <bankoleidris@gmail.com>
2021-09-05 22:01:27 +0000

    another commit

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // file system (FS) module
-const { readFileSync, writeFileSnc } = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
+
+// readFileSync reads content of a file
+const first = readFileSync("./content/first.txt", "utf-8");
+const second = readFileSync("./content/second.txt", "utf-8");
+console.log(first, second);
+
+// writeFileSync writes a new file
+writeFileSync(
+  "./content/result-sync.txt",
+  `Here is the result : ${first}, ${second}`
+);
