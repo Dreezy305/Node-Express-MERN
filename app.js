aa0954629b37f27db9eccdf9e3257b108fc93ef2
Dreezy305 <bankoleidris@gmail.com>
2021-09-05 20:31:37 +0000

    another commit

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const names = require("./4-names");
 console.log(names);
 const greet = require("./5-utils");
 const data = require("./6-alternative-flavor");
-console.log(data);
 
 greet("idris");
 //sayHi(secret);
