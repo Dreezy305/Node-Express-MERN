e7771be2462428473f29d8d306f3a819e98c3d2f
Dreezy305 <bankoleidris@gmail.com>
2021-09-05 20:40:11 +0000

    another commit

---
+++ 7-mind-grenade.js
@@ -0,0 +1,8 @@
+const num1 = 2;
+const num2 = 3;
+
+function addNum() {
+  console.log(`the sum is : ${num1 + num2}`);
+}
+
+addNum();
--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const names = require("./4-names");
 console.log(names);
 const greet = require("./5-utils");
 const data = require("./6-alternative-flavor");
+require("./7-mind-grenade");
 
-greet("idris");
+//greet("idris");
 //sayHi(secret);
-greet(names.john);
-greet(names.peter);
+//greet(names.john);
+//greet(names.peter);
