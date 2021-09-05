774beb79f8da3badc8d91acf67dff8664b78cd54
Dreezy305 <bankoleidris@gmail.com>
2021-09-05 21:30:38 +0000

    another commit

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ console.log(path.sep);
 
 // join method
 const filePath = path.join("/content", "subfolder", "test.txt");
-
 console.log(filePath);
+
+// base name
+const base = path.basename(filePath);
+console.log(base);
+
+// resolve gets the absolute path
+const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt");
+console.log(absolute);
