4a0cd7e4b281b8d3f0f896e33333fbc35e418248
Dreezy305 <bankoleidris@gmail.com>
2021-09-07 07:15:26 +0000

    loash

--- app.js
+++ app.js
@@ -7,3 +7,9 @@
 // global dependency - use it in any project
 // npm install -g <packageName>
 // sudo install -g <packageName> (linux, mac)
+
+const _ = require("lodash");
+
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
