dfb941c15e6a4b6a30f7ec46a2908c83f4228b46
Dreezy305 <bankoleidris@gmail.com>
2021-09-11 08:25:59 +0000

    event module

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // event programming
 const EventEmitter = require("events");
+
+const customEmitter = new EventEmitter();
+
+customEmitter.on("response", () => {
+  console.log("data recieved");
+});
+
+customEmitter.on("response", () => {
+  console.log("not good enough");
+});
+
+customEmitter.emit("response");
