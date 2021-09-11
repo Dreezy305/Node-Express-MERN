fb8536db219dbab2770df6364c41412c73f53418
Dreezy305 <bankoleidris@gmail.com>
2021-09-11 08:19:57 +0000

    event module

--- app.js
+++ app.js
@@ -1,27 +1,2 @@
-const { readFile } = require("fs");
-const util = require("util");
-
-//readFile("./content/first.txt", "utf-8", (err, data) => {
-//  if (err) {
-//    return;
-//  } else {
-//    console.log(data);
-//  }
-//});
-
-// async non-blocking pattern
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
-
-getText("./content/first.txt")
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+// event programming
+const EventEmitter = require("events");
